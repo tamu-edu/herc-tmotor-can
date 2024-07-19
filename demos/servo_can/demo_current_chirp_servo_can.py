4920d35f5aeede011f4b803511e70c3e92c190d3
Jed Simms <jedsimms7@tamu.edu>
2024-07-19 22:31:42 +0100

    work on the organization of the overall code

--- demos/servo_can/demo_current_chirp_servo_can.py
+++ demos/servo_can/demo_current_chirp_servo_can.py
@@ -4,22 +4,24 @@ from NeuroLocoMiddleware.SysID import Chirp
 #      from TMotorCANControl.TMotorManager import TMotorManager
 # except ModuleNotFoundError:
 from sys import path
-path.append("/home/pi/TMotorCANControl/src/")
+path.append("/home/pi/HERCLabDrive/herc-tmotor-can/TMotorCANControl/src/")
 from TMotorCANControl.servo_can import TMotorManager_servo_can
 import time
 
 
-def chirp_demo(dev, amp=1.0, dt=0.001):
+def chirp_demo(dev, amp=0.4, dt=0.001):
     print("Starting current chirp demo. Press ctrl+C to quit.")
-    chirp = Chirp(20, 1, 5)
+    chirp = Chirp(5, 0.5, 15)
     dev.enter_current_control()
     time.sleep(0.1)
 
     loop = SoftRealtimeLoop(dt = dt, report=True)
     for t in loop:
         dev.update()
-        dev.current_qaxis = amp*chirp.next(t) # a barely audible note
+        dev.current_qaxis = amp*chirp.next(t) - 8
+        print(dev.current_qaxis)
+         # a barely audible note
 
 with TMotorManager_servo_can(motor_type='AK80-9', motor_ID=0) as dev:
-    chirp_demo(dev, amp=3.0)
+    chirp_demo(dev, amp=8)
 print("done with chirp_demo()")
